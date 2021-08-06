80a26c60b393ae48ba7751af1d85b307602d069d
Nguyen Dang Nam <Nguyen Dang Nam>
2021-08-06 19:02:39 +0700

    Merged PR 681: fix environment
    fix environment

--- public/server/db.js
+++ public/server/db.js
@@ -1,11 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+const isDev = require("electron-is-dev");
 
-//dev
-//const storage = path.join(__dirname, "/database", "iqtree.sqlite3");
-
-//build
-const storage =path.join(process.resourcesPath, 'database', 'iqtree.sqlite3');
+const storage = isDev
+  ? path.join(__dirname, "/database", "iqtree.sqlite3")
+  : path.join(process.resourcesPath, "database", "iqtree.sqlite3");
 
 const db = new sqlite3.Database(storage);
 module.exports = db;
