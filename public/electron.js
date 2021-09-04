59198f3ca5583e7cf4058c19ec2a9b0e23e5cc86
Nam <Nam>
2021-09-04 22:32:11 +0700

    Merged PR 1222: close MAC
    close MAC

--- public/electron.js
+++ public/electron.js
@@ -294,10 +294,10 @@ function createWindow() {
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-    app.exit();
-  }
+  // if (process.platform !== "darwin") {
+  app.quit();
+  app.exit();
+  // }
 });
 app.on("activate", () => {
   if (mainWindow === null) {
