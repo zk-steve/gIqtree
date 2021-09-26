68d90e1c76225c5a38a5f153f8b4d5ac5f530e2e
Nam <Nam>
2021-09-26 23:00:31 +0700

    Merged PR 1786: vl that

--- public/server/controller/file_handler.js
+++ public/server/controller/file_handler.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
+const { filterName } = require("./project");
 
 module.exports.viewFile = (file_path) => {
     return new Promise((resolve, reject) => {
         fs.readFile(file_path, "utf-8", (err, data) => {
             if (err) reject(err);
-            resolve(data);
+            filterName(file_path)
+                .then(name => {
+                    resolve({data, name})
+                })
+                .catch(err => {
+                    reject({message: "Can not get file name", status: 0})
+                })
         })
     })
 }
